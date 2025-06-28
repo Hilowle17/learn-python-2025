ec211952237f71facd2a1130e622a02c7e7e9ca1
Mohamed Hilowle Ahmed <universityoperations2022@gmail.com>
2025-06-28 12:38:46 +0300

    Removed Day 1–4 folders from GitHub via .gitignore

--- Day 4 Data Structures/Challenge_3_Set.py
+++
@@ -1,9 +0,0 @@
-#Create a set of colors: {"red", "blue"}
-#Add "green", then remove "red", and print the final set.
-
-colors = {"Gaduud", "Madow", "Cadaan"}
-print("Original: ", colors)
-colors.add("Cagaar")
-print("After Adding: ", colors)
-colors.remove("Gaduud")
-print("After Removing: ", colors)
